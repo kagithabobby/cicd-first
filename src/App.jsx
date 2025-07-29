35c2aad83e4e3e4572a2801eae1773ee52f480a0
kagithabobby <bobbykagitha1980@gmail.com>
2025-07-29 12:01:33 +0530

    app.jsx second version

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,19 @@ function App() {
 
   return (
     <>
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <h1
-        style={{
-          transition: 'color 0.3s',
-          cursor: 'pointer'
-        }}
-        onMouseEnter={e => (e.target.style.color = '#0078d4')}
-        onMouseLeave={e => (e.target.style.color = '')}
-      >
-        CICD DevOps
-      </h1>
-    </div>
+      <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        <h1
+          style={{
+            transition: 'color 0.3s',
+            cursor: 'pointer'
+          }}
+          onMouseEnter={e => (e.target.style.color = '#0078d4')}
+          onMouseLeave={e => (e.target.style.color = '')}
+        >
+          CICD DevOps
+        </h1>
+        <h3>Cloud Computing</h3>
+      </div>
     </>
   )
 }
